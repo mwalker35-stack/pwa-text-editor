27f41a1a37d5bfb79356042e786528eae0090ab6
Marcus Walker <mwalke35@yahoo.com>
2023-04-01 15:15:59 -0400

    added code for client and server side.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,33 @@
-const butInstall = document.getElementById('buttonInstall');
+import { Workbox } from 'workbox-window';
+import Editor from './editor';
+import './database';
+import '../css/style.css';
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+const main = document.querySelector('#main');
+main.innerHTML = '';
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
+
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+}
